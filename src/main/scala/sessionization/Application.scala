73e40ebaa5c4f3bcccec48820fd072931b07a2b9
wattt3 <chleodnr3@gmail.com>
2024-10-30 17:31:54 +0900

    UDF 통한 sessionization 구현 - event_time 배열으로 session_id를 uuid로 할당하는 UDF 작성 - Sessionization 로직 수정   1. user_id 으로 Group By, 다른 모든 컬럼들을 collect_list 및 정렬 => events   2. UDF(assignSessionId) 통하여 event_times 를 기준으로 session id 할당 => session_ids   3. events 와 session_ids 를 zip 이후 explode 하여 원하는 결과 값 반환

--- src/main/scala/sessionization/Application.scala
+++ src/main/scala/sessionization/Application.scala
@@ -1,13 +1,14 @@
 package sessionization
 
-import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
-object Application {
+import java.sql.Timestamp
+import java.util.UUID
 
-  private val SESSION_EXPIRED_TIME: Int = 30 * 60
+object Application {
+  private val SESSION_EXPIRED_TIME: Long = 30 * 60 * 1000L
   private val SCHEMA: StructType = StructType(
     Seq(
       StructField("event_time", StringType),
@@ -20,63 +21,91 @@ object Application {
       StructField("user_id", LongType)
     )
   )
+  private val session = SparkSession
+    .builder()
+    .appName("Sessionization")
+    .master("local[*]")
+    .getOrCreate()
+
+  import session.implicits._
 
   def main(args: Array[String]): Unit = {
-    val session = SparkSession
-      .builder()
-      .appName("Sessionization")
-      .master("local[*]")
-      .getOrCreate()
-
     val df = session.read
       .option("header", value = true)
       .schema(SCHEMA)
       .csv("../samples/2019-Oct.csv")
       .withColumn(
         "event_time",
-        to_timestamp(col("event_time"), "yyyy-MM-dd HH:mm:ss 'UTC'")
+        to_timestamp($"event_time", "yyyy-MM-dd HH:mm:ss 'UTC'")
       )
 
-    val dfWithSessionId = augmentSessionId(df)
+    augmentSessionId(df)
 
     session.stop()
   }
 
-  def augmentSessionId(
-      df: Dataset[Row]
-  ): Dataset[Row] = {
-    val windowSpec = Window.partitionBy(col("user_id")).orderBy("event_time")
+  def augmentSessionId(df: Dataset[Row]): Dataset[Row] = {
+    val events = struct(
+      $"event_time",
+      $"event_type",
+      $"product_id",
+      $"category_id",
+      $"category_code",
+      $"brand",
+      $"price"
+    )
 
-    val dfWithTimeDiff = df
+    // UDF: 세션화 로직을 수행하여 세션 ID(uuid)를 할당
+    val assignSessionId = udf((event_times: Seq[Timestamp]) => {
+      val headSessionIds = Seq[String](UUID.randomUUID().toString)
+      val headTime = event_times.head
+
+      // 각 이벤트 시간에 대해 sessionId 할당, SESSION_EXPIRED_TIME 초과 시 새로운 sessionId 생성
+      event_times.tail
+        .foldLeft((headSessionIds, headTime)) {
+          case ((sessionIds, prevTime), currentTime) =>
+            val sessionId =
+              if (currentTime.getTime - prevTime.getTime > SESSION_EXPIRED_TIME)
+                UUID.randomUUID().toString
+              else
+                sessionIds.last
+            (sessionIds :+ sessionId, currentTime)
+        }
+        ._1
+    })
+
+    /* 각 사용자별로 그룹화하고 세션 ID 할당
+     * 1. user_id 으로 Group By, 다른 모든 컬럼들을 collect_list 및 정렬 => events
+     * 2. UDF(assignSessionId) 통하여 event_times 를 기준으로 session id 할당 => session_ids
+     * 3. events 와 session_ids 를 zip 이후 explode 하여 원하는 결과 값 반환
+     */
+    df.groupBy("user_id")
+      .agg(array_sort(collect_list(events)).as("events"))
+      .withColumn("event_times", $"events.event_time")
+      .withColumn("session_ids", assignSessionId($"event_times"))
       .withColumn(
-        "ts_diff",
-        unix_timestamp(col("event_time")) - unix_timestamp(
-          lag(col("event_time"), 1).over(windowSpec)
+        "events_with_session",
+        zip_with(
+          $"events",
+          $"session_ids",
+          (e, s) =>
+            struct(
+              e.getField("event_time").as("event_time"),
+              e.getField("event_type").as("event_type"),
+              e.getField("product_id").as("product_id"),
+              e.getField("category_id").as("category_id"),
+              e.getField("category_code").as("category_code"),
+              e.getField("brand").as("brand"),
+              e.getField("price").as("price"),
+              s.as("session_id")
+            )
         )
       )
-      .withColumn(
-        "ts_diff",
-        when(
-          row_number().over(windowSpec) === 1 || col(
-            "ts_diff"
-          ) > SESSION_EXPIRED_TIME,
-          -1
-        ).otherwise(col("ts_diff"))
+      .select(
+        $"user_id",
+        explode($"events_with_session").as("event_with_session")
       )
-
-    val dfWithSessionId = dfWithTimeDiff
-      .withColumn(
-        "session_id",
-        when(col("ts_diff") === -1, uuid()).otherwise(null)
-      )
-      .withColumn(
-        "session_id",
-        last("session_id", ignoreNulls = true)
-          .over(windowSpec.rowsBetween(Window.unboundedPreceding, 0))
-      )
-      .drop("ts_diff")
-
-    dfWithSessionId
+      .select($"user_id", $"event_with_session.*")
   }
 
 }
--- src/test/scala/sessionization/ApplicationTest.scala
+++ src/test/scala/sessionization/ApplicationTest.scala
@@ -5,6 +5,8 @@ import org.apache.spark.sql.functions.{col, to_timestamp}
 import org.scalatest.flatspec.AnyFlatSpec
 import sessionization.Application.augmentSessionId
 
+import java.sql.Timestamp
+
 class ApplicationTest extends AnyFlatSpec {
 
   val spark: SparkSession = SparkSession
@@ -17,28 +19,154 @@ class ApplicationTest extends AnyFlatSpec {
 
   "SessionId" should "generate unique session IDs for time gaps over SESSION_EXPIRED_TIME" in {
     // given
-    val sample = Seq(
-      ("1", "2019-10-01 10:00:00"),
-      ("1", "2019-10-01 10:15:00"),
-      ("1", "2019-10-01 11:00:00"), // 새로운 세션 시작 (30분 초과)
-      ("1", "2019-10-01 11:10:00"),
-      ("2", "2019-10-01 10:00:00"), // 새로운 user_id
-      ("2", "2019-10-01 10:20:00"),
-      ("2", "2019-10-01 11:30:00") // 새로운 세션 시작 (30분 초과)
-    ).toDF("user_id", "event_time")
-      .withColumn(
-        "event_time",
-        to_timestamp(col("event_time"), "yyyy-MM-dd HH:mm:ss")
+    val data = Seq(
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 10:00:00"),
+        "click",
+        "prod1",
+        "cat1",
+        "code1",
+        "brand1",
+        10.0
+      ),
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 10:15:00"),
+        "click",
+        "prod2",
+        "cat1",
+        "code1",
+        "brand2",
+        12.0
+      ),
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 11:00:00"),
+        "purchase",
+        "prod3",
+        "cat2",
+        "code2",
+        "brand1",
+        15.0
+      ),
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 11:10:00"),
+        "click",
+        "prod4",
+        "cat2",
+        "code2",
+        "brand2",
+        14.0
+      ),
+      (
+        "2",
+        Timestamp.valueOf("2019-10-01 10:00:00"),
+        "click",
+        "prod5",
+        "cat3",
+        "code3",
+        "brand3",
+        20.0
+      ),
+      (
+        "2",
+        Timestamp.valueOf("2019-10-01 10:20:00"),
+        "click",
+        "prod6",
+        "cat3",
+        "code3",
+        "brand1",
+        22.0
+      ),
+      (
+        "2",
+        Timestamp.valueOf("2019-10-01 11:30:00"),
+        "purchase",
+        "prod7",
+        "cat4",
+        "code4",
+        "brand3",
+        25.0
       )
+    ).toDF(
+      "user_id",
+      "event_time",
+      "event_type",
+      "product_id",
+      "category_id",
+      "category_code",
+      "brand",
+      "price"
+    ).withColumn(
+      "event_time",
+      to_timestamp(col("event_time"), "yyyy-MM-dd HH:mm:ss")
+    )
 
     // when
-    val result = augmentSessionId(sample).collect()
+    val result = augmentSessionId(data).collect()
 
     // then
-    assert(result(0).getString(2) == result(1).getString(2))
-    assert(result(1).getString(2) != result(2).getString(2))
-    assert(result(2).getString(2) == result(3).getString(2))
-    assert(result(4).getString(2) == result(5).getString(2))
-    assert(result(5).getString(2) != result(6).getString(2))
+    assert(result(0).getString(8) == result(1).getString(8))
+    assert(result(1).getString(8) != result(2).getString(8))
+    assert(result(2).getString(8) == result(3).getString(8))
+    assert(result(4).getString(8) == result(5).getString(8))
+    assert(result(5).getString(8) != result(6).getString(8))
+  }
+
+  "UnorderedData" should "generate correct session id" in {
+    // given
+    val data = Seq(
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 10:00:00"),
+        "click",
+        "prod1",
+        "cat1",
+        "code1",
+        "brand1",
+        10.0
+      ),
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 11:00:00"),
+        "purchase",
+        "prod3",
+        "cat2",
+        "code2",
+        "brand1",
+        15.0
+      ),
+      (
+        "1",
+        Timestamp.valueOf("2019-10-01 10:15:00"),
+        "click",
+        "prod2",
+        "cat1",
+        "code1",
+        "brand2",
+        12.0
+      )
+    ).toDF(
+      "user_id",
+      "event_time",
+      "event_type",
+      "product_id",
+      "category_id",
+      "category_code",
+      "brand",
+      "price"
+    ).withColumn(
+      "event_time",
+      to_timestamp(col("event_time"), "yyyy-MM-dd HH:mm:ss")
+    )
+
+    // when
+    val result = augmentSessionId(data).collect()
+
+    // then
+    assert(result(0).getString(8) == result(1).getString(8))
+    assert(result(1).getString(8) != result(2).getString(8))
   }
 }
